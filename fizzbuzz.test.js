e83dd3070c048dcda22f07eb3b3563b98c4f7485
Jayleaf1125 <jasonpadilla104@gmail.com>
2022-07-03 21:58:38 -0400

    Two of Eight Test Created and Successful

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,5 +1,23 @@
 const fizzBuzz = require('./fizzbuzz')
 
+// Write a test for a function called fizzbuzz that, given the number 1, it should return 1.
 test('Should return 1', () => {
     expect(fizzBuzz(1)).toBe(1)
 })
+
+// Write a test that fizzbuzz, given 2, should return 2.
+test('Should return 2', () => {
+    expect(fizzBuzz(2)).toBe(2)
+})
+
+// Write a test that fizzbuzz, given 3, should return 'Fizz'.
+
+// We can skip a test for 4, since we can be pretty sure that it will pass. Let's instead write a test for 5. Given 5, fizzbuzz should return 'Buzz'.
+
+// At '6', we'll introduce another requirement: multiples of 3 should also return Fizz. Write a test that given 6, fizzbuzz should return 'Fizz'.
+
+// We can skip the tests for 6, 7, 8, and 9 since we're already testing those cases. For our next test, write a test that given a multiple of 5, we should return 'Buzz'.
+
+// Write a test that, given 15, fizzbuzz returns "FizzBuzz".
+
+// Write a test that, given multiple of 15, returns "FizzBuzz"
